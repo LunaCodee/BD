8ac728d83ce7a3bbe67598dd5448b41d17cf5c4d
LunaCodee <laura.zumbakiene@gmail.com>
2023-06-28 21:46:09 +0300

    user part

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = mongoose.Schema({
     name: { type: String, required: true, minlength: 3 },
     email: { type: String, required: true, minlength: 8 },
     password: { type: String, required: true, minlength: 6 },
+    asked_questions_ids: { type: Array, required: true  },
 });
 
 module.exports = mongoose.model("User", userSchema);
