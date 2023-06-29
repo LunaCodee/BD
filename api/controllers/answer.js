30b7e793dcb93a018ce0de4d36557a2bbc40961c
LunaCodee <laura.zumbakiene@gmail.com>
2023-06-29 18:02:29 +0300

    like/dislike

--- api/controllers/answer.js
+++ api/controllers/answer.js
@@ -5,22 +5,20 @@ const uniqid = require("uniqid");
 module.exports.CREATE_ANSWER = async (req, res) => {
   try {
 
-    const id = req.params.id;
-    const answer_text = req.body.answer_text;
-    // const { id } = req.params;
-    // const { answer_text } = req.body;
+    const questionId = req.params.id;
+    const answerText = req.body.answer_text;
 
-    const question = await QuestionModel.findById(id);
+    const question = await QuestionModel.findById(questionId);
     if (!question) {
       return res.status(404).json({ response: "Question not found" });
     }
 
     const answer = new AnswerModel({
-      id: uniqid(),
-      answer_text,
-      gained_likes_number: 0,
+        id: uniqid(),
+        answer_text: answerText,
+        gained_likes_number: 0,
     });
-
+ 
     question.answers_ids.push(answer.id);
 
     await answer.save();
@@ -32,6 +30,75 @@ module.exports.CREATE_ANSWER = async (req, res) => {
     return res.status(500).json({ response: "ERROR" });
   }
 };
+
+module.exports.CREATE_ANSWER = async (req, res) => {
+    try {
+      const questionId = req.params.id;
+      const answerText = req.body.answer_text;
+  
+      const question = await QuestionModel.findById(questionId);
+      if (!question) {
+        return res.status(404).json({ response: "Question not found" });
+      }
+  
+      const answer = new AnswerModel({
+        id: uniqid(),
+        answer_text: answerText,
+        gained_likes_number: 0,
+      });
+  
+      question.answers_ids.push(answer.id);
+  
+      await answer.save();
+      await question.save();
+  
+      return res.status(201).json({ response: "Answer was created" });
+    } catch (error) {
+      console.log("err", error);
+      return res.status(500).json({ response: "ERROR" });
+    }
+  };
+  
+  module.exports.LIKE_ANSWER = async (req, res) => {
+    try {
+      const answerId = req.params.id;
+  
+      const answer = await AnswerModel.findById(answerId);
+      if (!answer) {
+        return res.status(404).json({ response: "Answer not found" });
+      }
+  
+      // Increase the gained_likes_number by 1
+      answer.gained_likes_number += 1;
+  
+      await answer.save();
+  
+      return res.status(200).json({ response: "Answer was liked" });
+    } catch (error) {
+      console.log("err", error);
+      return res.status(500).json({ response: "ERROR" });
+    }
+  };
+  
+  module.exports.DISLIKE_ANSWER = async (req, res) => {
+    try {
+      const answerId = req.params.id;
+  
+      const answer = await AnswerModel.findById(answerId);
+      if (!answer) {
+        return res.status(404).json({ response: "Answer not found" });
+      }
+  
+      answer.gained_likes_number -= 1;
+  
+      await answer.save();
+  
+      return res.status(200).json({ response: "Answer was disliked" });
+    } catch (error) {
+      console.log("err", error);
+      return res.status(500).json({ response: "ERROR" });
+    }
+  };
 
 // module.exports.GET_ALL_QUESTION_ANSWERS = async (req, res) => {
 //   try {
--- api/routes/answer.js
+++ api/routes/answer.js
@@ -3,11 +3,15 @@ const router = express.Router();
 // const authMiddleware = require("../middleware/auth");
 const {
   CREATE_ANSWER,
+  LIKE_ANSWER,
+  DISLIKE_ANSWER,
 //   GET_ALL_QUESTION_ANSWERS,
 //   DELETE_ANSWER_BY_ID, 
 } = require("../controllers/answer");
 
 router.post("/question/:id/answers", CREATE_ANSWER);
+router.post("/answers/:id/like", LIKE_ANSWER);
+router.post("/answers/:id/dislike", DISLIKE_ANSWER);
 // router.get("/question/:id/answers", GET_ALL_QUESTION_ANSWERS);
 // router.delete("/answer/:id",  DELETE_ANSWER_BY_ID);
 
